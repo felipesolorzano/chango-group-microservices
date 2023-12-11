8e6ef17beb1d8c8aa8e0054ad673542a8b8be551
felipe solorzano <felipe.de.jesus.solorzano@gmail.com>
2023-12-11 17:50:28 -0600

    up

--- build/server.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify("from build server Hello from Lambda and Github! test"),
-    }
-    return response
-  }

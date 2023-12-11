02608ca76196d85b9d6e14b8635247c26d7d69f5
felipe solorzano <felipe.de.jesus.solorzano@gmail.com>
2023-12-11 15:53:21 -0600

    up

--- build/server.js
+++ build/server.js
@@ -1,4 +1,6 @@
-exports.handler = async (event) => {
+ 
+ require('prettier')
+ exports.handler = async (event) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify("Hello from Lambda and Github! from local"),
